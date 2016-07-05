32b5ce671641b2bbc8a2929d45d336b5a905f621
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-05 13:01:31 -0400

    init

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'); // npm install gulp
+var sass = require('gulp-sass'); // npm install gulp-sass
+var browserify = require('gulp-browserify') // npm install gulp-browserify
+var htmlmin = require('gulp-htmlmin');
+
+
+gulp.task('default', ['html','css', 'js'])
+
+gulp.task('css', function (){
+  gulp.src('./sass/styles.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./public'))
+});
+
+gulp.task('html', function(){
+  gulp.src('./index.html')
+    // .pipe(htmlhint())
+    .pipe(htmlmin({collapseWhitespace:true}))
+    .pipe(gulp.dest('./public'))
+});
+
+gulp.task('js', function(){
+  gulp.src('./js/*.js')
+    .pipe(browserify())
+    .pipe(gulp.dest('./public'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./sass/*.scss', ['css']);
+  gulp.watch('./index.html', ['html']);
+  gulp.watch('./js/*.js', ['js']);
+  gulp.watch('./js/*/*.js', ['js']);
+});
