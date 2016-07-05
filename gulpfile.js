2ed0d5c4574099e908a3d6499011d1cd5f59509e
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-05 16:48:23 -0400

    working on score

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp'); // npm install gulp
-var sass = require('gulp-sass'); // npm install gulp-sass
+// var sass = require('gulp-sass'); // npm install gulp-sass
 var browserify = require('gulp-browserify') // npm install gulp-browserify
 var htmlmin = require('gulp-htmlmin');
 
 
-gulp.task('default', ['html','css', 'js'])
+gulp.task('default', ['html', 'js'])
 
-gulp.task('css', function (){
-  gulp.src('./main.css')
-  .pipe(sass())
-  .pipe(gulp.dest('./public'))
-});
+// gulp.task('css', function (){
+//   gulp.src('./main.css')
+//   .pipe(sass())
+//   .pipe(gulp.dest('./public'))
+// });
 
 gulp.task('html', function(){
   gulp.src('./index.html')
@@ -20,14 +20,14 @@ gulp.task('html', function(){
 });
 
 gulp.task('js', function(){
-  gulp.src('./*.js')
+  gulp.src('./main.js')
     .pipe(browserify())
     .pipe(gulp.dest('./public'))
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./*.css', ['css']);
+  // gulp.watch('./*.css', ['css']);
   gulp.watch('./index.html', ['html']);
-  gulp.watch('./*.js', ['js']);
+  gulp.watch('./main.js', ['js']);
   // gulp.watch('./js/*/*.js', ['js']);
 });
